a61a41eb43a5a5e1a06f85e2bdfd6c3f7bb6ae38
haneman224 <ka1an2ta@gmail.com>
2023-09-06 20:48:33 +0900

    とりあえずの形。

---
+++ mein.js
@@ -0,0 +1,5 @@
+'use strict';
+//document.getElementById(<label><input type="radio" name="2">完了！</label>) 惜しい。
+// p id ="test"でクラスをあげてあればよかった。
+//let text = document.getElementById('test').textContent; //宣言をしてあげる
+document.getElementById('test').textContent = '完了！';
