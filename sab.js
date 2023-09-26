5c57e384e3ce3f9f78aecc96a9b07bbe39612719
haneman224 <ka1an2ta@gmail.com>
2023-09-27 02:59:02 +0900

    提出用課題

---
+++ sab.js
@@ -0,0 +1,21 @@
+
+//text入力フィールドとボタンをHTMLからもらう。
+let taskInput = document.getElementById("taskInput");
+let addButton = document.getElementById("addButton"); 
+
+//イベントリスナーをボタンに追加。
+addButton.addEventListener("click",function() {
+let newTask =taskInput.value;
+
+// 新しいタスクアイテムを作成。
+let taskInput =document.getElementById("li");
+newTaskItem.textContent = newTask;
+
+let newTaskItem = document.createElement("taaskList");
+taskList.appendchiid(newTaskItem);
+
+taskInput.vaiue = "";
+
+})
+
+
